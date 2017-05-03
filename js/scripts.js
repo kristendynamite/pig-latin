9342689c135813f2f5373bf7beb94de808e467ad
Jared Luedtke and Kristen McKee <jared+kristen@>
2017-05-03 13:12:45 -0700

    add JS for spec: multiple consonants up to three total

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,27 @@ var vowel = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
 var translate = function(textArray) {
   if (textArray.length === 1) {
+    console.log("That was a one letter word")
   } else if ((textArray[0] === "a") || (textArray[0] === "e") || (textArray[0] === "i") || (textArray[0] === "o") || (textArray[0] === "u") || (textArray[0] === "A") || (textArray[0] === "E") || (textArray[0] === "I") || (textArray[0] === "O") || (textArray[0] === "U")) {
+    textArray.push("w");
+  } else if (textArray.length === 2) {
+    textArray.push(textArray[0]);
+    textArray.shift();
   } else {
     textArray.push(textArray[0]);
     textArray.shift();
       if ((textArray[0] !== "a") || (textArray[0] !== "e") || (textArray[0] !== "i") || (textArray[0] !== "o") || (textArray[0] !== "u") || (textArray[0] !== "A") || (textArray[0] !== "E") || (textArray[0] !== "I") || (textArray[0] !== "O") || (textArray[0] !== "U")) {
         textArray.push(textArray[0]);
         textArray.shift();
-
+        if ((textArray[0] !== "a") || (textArray[0] !== "e") || (textArray[0] !== "i") || (textArray[0] !== "o") || (textArray[0] !== "u") || (textArray[0] !== "A") || (textArray[0] !== "E") || (textArray[0] !== "I") || (textArray[0] !== "O") || (textArray[0] !== "U")) {
+          textArray.push(textArray[0]);
+          textArray.shift();
+        }
     }
   };
 
   textArray.push("ay");
-  return textArray;
+  return textArray.join("");
 };
 
 
